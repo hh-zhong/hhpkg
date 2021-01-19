580e54bc52f73b2a55fd92b67b8b777e99e73ddd
hh-zhong <lehang.zhong@gmail.com>
2021-01-19 17:23:15 -0500

    Initial commit

---
+++ R/dat_rn.R
@@ -0,0 +1,17 @@
+#' Example data
+#'
+#' Data from rnorm
+#'
+#' @docType data
+#' @format A data frame with 10 rows and 10 variables:
+#' \describe{
+#'   \item{X1}{Litter ID}
+#'   \item{X2}{Dose of ethylene glycol (EG) administered}
+#'   \item{X3}{Fetal weight}
+#'   \item{X4}{Indicator for fetal malformation}
+#' }
+#' @source http://users.stat.ufl.edu/~aa/
+#' @references Agresti, Alan.
+#' Categorical data analysis. Vol. 482.
+#' John Wiley & Sons, 2003.Harvard
+"dat_rn"
---
+++ R/hhf1.R
@@ -0,0 +1,13 @@
+#' @title Max
+#' @description a function that returns maximum value
+#' @param x numeric vector
+#' @return y
+#' @author Lehang Zhong
+#' @examples
+#' max_hhz(c(1,4,NA,10,-500))
+#' @export
+
+max_hhz <- function(x){
+  y = max( x,na.rm = T)
+  return(y)
+}
